756f629802df2170e0647538d131b35c98ddfbc3
InvisibleAstra <aleks.kalinichenko989@gmail.com>
2023-01-10 16:30:12 +0300

    add component todo list

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,42 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {TodoListComponent} from "./components/TodoList/TodoList.component";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    let TodoListsData = [{
+        id: 'dsafdf',
+        title: 'What to learn',
+        tasks: [{
+            id: "123sd",
+            title: "js",
+            isDone: true,
+        }, {
+            id: "123sd",
+            title: "js",
+            isDone: false,
+        }]
+    }, {
+        id: 'sdfsd12',
+        title: 'What to learn2',
+        tasks: [{
+            id: "123sd",
+            title: "js",
+            isDone: false,
+        }, {
+            id: "123sd",
+            title: "js",
+            isDone: true,
+        }]
+    },];
+    let TodoLists = TodoListsData.map(value => <TodoListComponent key={value.id} tasks={value.tasks}
+                                                                  title={value.title}/>);
+
+    return (
+        <div className='flex'>
+            {TodoLists}
+        </div>
+    )
 }
 
 export default App;
